f51f971007ca72290fba862650d18e19fa315856
Kozlov Sergey <skozlov@usetech.ru>
2022-05-18 16:48:27 +0300

    feat: add react query

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,28 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import AppRoutes from './AppRoutes';
 import { GlobalStyle } from './styles';
 
 import './styles/variables.scss';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 ReactDOM.render(
   <StrictMode>
     <GlobalStyle />
-    <AppRoutes />
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>,
   document.getElementById('root'),
 );
