63baf5620f419f17e0c15caeeeaafbd510e7d3d8
pavlyxar <pavlyxar567@inbox.ru>
2022-05-19 15:37:59 +0300

    fix: use default object config instead of basic condition

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -1,7 +1,7 @@
 import { Collection, SchemaVersion } from '@app/api/graphQL';
+import config from '@app/config';
 
-const IPFS_GATEWAY =
-  window.ENV?.REACT_APP_IPFS_GATEWAY || process.env.REACT_APP_IPFS_GATEWAY;
+const { IPFSGateway } = config;
 
 export const getCollectionCoverUri = (collection: Collection): string => {
   const defaultCollectionCoverId = '1';
@@ -9,8 +9,8 @@ export const getCollectionCoverUri = (collection: Collection): string => {
     ['ImageURL', collection.offchain_schema.replace('{id}', defaultCollectionCoverId)],
     [
       'Unique',
-      IPFS_GATEWAY && collection?.variable_on_chain_schema?.collectionCover
-        ? `${IPFS_GATEWAY}/${collection.variable_on_chain_schema.collectionCover}`
+      IPFSGateway && collection?.variable_on_chain_schema?.collectionCover
+        ? `${IPFSGateway}/${collection.variable_on_chain_schema.collectionCover}`
         : null,
     ],
   ]);
