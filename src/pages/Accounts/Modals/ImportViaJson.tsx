4d91938dd77032094834d169fba9ac86bd7e6ca8
4ndrewBaker <anr.andrew@gmail.com>
2022-05-23 11:34:17 +0400

    [WMS-867] modal: add via json. (#27)
    - Add common styledComponents;
    - Fix/rename modal contents;
    
    Co-authored-by: Andrei Ov <aovsyannikov@usetech.com>

--- src/pages/Accounts/Modals/ImportViaJson.tsx
+++ src/pages/Accounts/Modals/ImportViaJson.tsx
@@ -1,10 +1,18 @@
 import React, { FC, useCallback, useState } from 'react';
-import { Button, Heading, Modal, Text } from '@unique-nft/ui-kit';
-import styled from 'styled-components/macro';
+import { Button, Heading, Modal } from '@unique-nft/ui-kit';
 import { KeyringPair } from '@polkadot/keyring/types';
 import keyring from '@polkadot/ui-keyring';
 
 import { PasswordInput, Upload } from '@app/components';
+import {
+  AdditionalText,
+  ContentRow,
+  LabelText,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  TextWarning,
+} from '@app/pages/Accounts/Modals/commonComponents';
 
 import { TCreateAccountModalProps } from './types';
 import { convertToU8a, keyringFromFile } from '../../../utils/jsonUtils';
@@ -25,7 +33,7 @@ export const ImportViaJSONAccountModal: FC<TCreateAccountModalProps> = ({
            setPair(keyringFromFile(data, rawRpcApi.genesisHash.toHex()));
          }
        };
- 
+
        reader.readAsArrayBuffer(file);
      }, */
   }, []);
@@ -42,66 +50,46 @@ export const ImportViaJSONAccountModal: FC<TCreateAccountModalProps> = ({
 
   return (
     <Modal isVisible={isVisible} isClosable={true} onClose={onFinish}>
-      <Content>
-        <Heading size="2">{'Add an account via backup JSON file'}</Heading>
-      </Content>
-      <InputWrapper>
-        <Text size={'m'}>Upload</Text>
-        <Text size={'s'} color={'grey-500'}>
-          Click to select or drop the file here
-        </Text>
-        <Upload onChange={onUploadChange} />
-      </InputWrapper>
-      <InputWrapper>
-        <Text size={'m'}>Password</Text>
-        <Text size={'s'} color={'grey-500'}>
-          The password that was previously used to encrypt this account
-        </Text>
-        <PasswordInput placeholder={'Password'} value={password} onChange={setPassword} />
-      </InputWrapper>
-      <TextStyled color="additional-warning-500" size="s">
-        Consider storing your account in a signer such as a browser extension, hardware
-        device, QR-capable phone wallet (non-connected) or desktop application for optimal
-        account security. Future versions of the web-only interface will drop support for
-        non-external accounts, much like the IPFS version.
-      </TextStyled>
-      <ButtonWrapper>
+      <ModalHeader>
+        <Heading size="2">Add an account via backup JSON file</Heading>
+      </ModalHeader>
+      <ModalContent>
+        <ContentRow>
+          <LabelText size={'m'}>Upload</LabelText>
+          <AdditionalText size={'s'} color={'grey-500'}>
+            Click to select or drop the file here
+          </AdditionalText>
+          <Upload onChange={onUploadChange} />
+        </ContentRow>
+        <ContentRow>
+          <LabelText size={'m'}>Password</LabelText>
+          <AdditionalText size={'s'} color={'grey-500'}>
+            The password that was previously used to encrypt this account
+          </AdditionalText>
+          <PasswordInput
+            placeholder={'Password'}
+            value={password}
+            onChange={setPassword}
+          />
+        </ContentRow>
+        <ContentRow>
+          <TextWarning color="additional-warning-500" size="s">
+            Consider storing your account in a signer such as a browser extension,
+            hardware device, QR-capable phone wallet (non-connected) or desktop
+            application for optimal account security. Future versions of the web-only
+            interface will drop support for non-external accounts, much like the IPFS
+            version.
+          </TextWarning>
+        </ContentRow>
+      </ModalContent>
+      <ModalFooter>
         <Button
           disabled={!password || !pair}
           role="primary"
           title="Restore"
           onClick={onRestoreClick}
         />
-      </ButtonWrapper>
+      </ModalFooter>
     </Modal>
   );
 };
-
-const Content = styled.div`
-  && h2 {
-    margin-bottom: 0;
-  }
-`;
-
-const InputWrapper = styled.div`
-  padding: var(--prop-gap) 0;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: var(--prop-gap);
-  row-gap: var(--prop-gap);
-`;
-
-const TextStyled = styled(Text)`
-  box-sizing: border-box;
-  display: flex;
-  padding: 8px 16px;
-  margin: calc(var(--prop-gap) * 1.5) 0;
-  border-radius: 4px;
-  background-color: var(--color-additional-warning-100);
-  width: 100%;
-`;
-
-const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`;
