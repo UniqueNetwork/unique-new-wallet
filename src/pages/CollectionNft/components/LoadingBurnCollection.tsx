13ec29ca9e276078696b4c6da2d9c96ed61c2009
Kozlov Sergey <skozlov@usetech.ru>
2022-05-31 16:35:08 +0300

    [WMS-54] - Страница Collection - Settings

--- src/pages/CollectionNft/components/LoadingBurnCollection.tsx
+++ src/pages/CollectionNft/components/LoadingBurnCollection.tsx
@@ -3,6 +3,6 @@ import { Heading, Modal, Loader } from '@unique-nft/ui-kit';
 export const LoadingBurnCollection = () => (
   <Modal isVisible={true}>
     <Heading>Please wait</Heading>
-    <Loader label={'Burning collection'} />
+    <Loader label="Burning collection" />
   </Modal>
 );
