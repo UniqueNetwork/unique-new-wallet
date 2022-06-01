935a8ce7dd3e509239b5e9c576efbe5ed3b6e571
Kozlov Sergey <skozlov@usetech.ru>
2022-06-01 17:58:21 +0300

    [WMS-869] - Модалка статуса транзакции

--- src/pages/CollectionNft/components/LoadingBurnCollection.tsx
+++
@@ -1,8 +0,0 @@
-import { Heading, Modal, Loader } from '@unique-nft/ui-kit';
-
-export const LoadingBurnCollection = () => (
-  <Modal isVisible={true}>
-    <Heading>Please wait</Heading>
-    <Loader label="Burning collection" />
-  </Modal>
-);
