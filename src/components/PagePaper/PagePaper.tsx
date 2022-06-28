685efd2f625f993f141acbf4b84c6881b042de4a
Andrey O <aovsyannikov@usetech.com>
2022-06-28 15:24:55 +0400

    [WAL-35] layout fix

--- src/components/PagePaper/PagePaper.tsx
+++ src/components/PagePaper/PagePaper.tsx
@@ -5,7 +5,6 @@ export const PagePaperStyles = css`
   box-shadow: 0 4px 12px rgb(0 0 0 / 8%);
   border-radius: 4px;
   flex: 1 1 auto;
-  display: flex;
 
   @media (max-width: 1024px) {
     background: var(--color-additional-light);
