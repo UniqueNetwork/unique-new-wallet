f542a9bb3bc82f1d2f3905e49a3ae1bc5c9fd357
Kozlov Sergey <skozlov@usetech.ru>
2022-06-29 16:42:53 +0300

    fix: [WAL-354] - fix amount

--- src/api/restApi/balance/queries/AccountBalanceQuery.ts
+++ src/api/restApi/balance/queries/AccountBalanceQuery.ts
@@ -22,7 +22,34 @@ export class AccountBalanceQuery extends EndpointQuery<AllBalancesResponse, Requ
     return ['account', 'balance', address];
   }
 
+  /*
+    Нам на вход приходит строка вида 123087.38808524234
+    Необходимо после точки показывать только первые 4 цифры
+   */
+  private static sliceAmount(balance: string) {
+    const arrBalance = balance.split('.');
+
+    if (balance.length === 1) {
+      return balance;
+    }
+    const lastElem = arrBalance.length - 1;
+    arrBalance[lastElem] = arrBalance[lastElem].slice(0, 4);
+    return arrBalance.join('.');
+  }
+
   request(api: IBaseApi, { address }: RequestArgs): Promise<AllBalancesResponse> {
-    return api.get(`${this.baseUrl}?address=${address}`);
+    return api
+      .get<AllBalancesResponse>(`${this.baseUrl}?address=${address}`)
+      .then((balance) => {
+        const keys = Object.keys(balance) as (keyof AllBalancesResponse)[];
+
+        keys.forEach((property) => {
+          balance[property].amount = AccountBalanceQuery.sliceAmount(
+            balance[property].amount,
+          );
+        });
+        return Promise.resolve(balance);
+      })
+      .catch(Promise.reject);
   }
 }
