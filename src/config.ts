29df5212ec1716fcdcdcfae67ef5d2c39b764033
pavlyxar <pavlyxar567@inbox.ru>
2022-07-04 18:15:49 +0300

    feat: delete useless envs

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,17 @@
 declare type Env = {
-  REACT_APP_UNIQUE_COLLECTION_IDS: string | undefined;
-  REACT_APP_IPFS_GATEWAY: string | undefined;
-  REACT_APP_IMAGE_SERVER_URL: string | undefined;
-  REACT_APP_UNIQUE_API_URL: string | undefined;
-  REACT_APP_SCAN_ACCOUNT_URL: string | undefined;
-  REACT_APP_HASURA_API_URL: string | undefined;
-} & Record<string, string | undefined>;
+  IPFS_GATEWAY_URL: string | undefined;
+  IMAGE_SERVER_URL: string | undefined;
+  SCAN_ACCOUNT_URL: string | undefined;
+  NET_QUARTZ_REST_API_URL: string | undefined;
+  HASURA_API_URL: string | undefined;
+};
 
 declare type Config = {
-  quartzRestApiUrl: string | undefined;
-  uniqueApiUrl: string | undefined;
-  uniqueRestApiUrl: string | undefined;
-  scanUrl: string | undefined;
-  imageServerUrl: string | undefined;
   IPFSGateway: string | undefined;
+  imageServerUrl: string | undefined;
+  scanUrl: string | undefined;
+  quartzRestApiUrl: string | undefined;
   hasuraApiUrl: string | undefined;
-  // chains: Record<string, Chain>;
 };
 
 declare global {
@@ -26,14 +22,11 @@ declare global {
 
 const config: Config = {
   // defaultChain: '',
-  quartzRestApiUrl:
-    window.ENV?.REACT_APP_NET_QUARTZ_REST_API_URL ||
-    process.env.REACT_APP_NET_QUARTZ_REST_API_URL,
-  uniqueApiUrl: window.ENV?.UNIQUE_API_URL || process.env.REACT_APP_UNIQUE_API_URL,
-  uniqueRestApiUrl: window.ENV?.REST_API_URL || process.env.REACT_APP_REST_API_URL,
+  IPFSGateway: window.ENV?.IPFS_GATEWAY_URL || process.env.REACT_APP_IPFS_GATEWAY_URL,
   imageServerUrl: window.ENV?.IMAGE_SERVER_URL || process.env.REACT_APP_IMAGE_SERVER_URL,
-  IPFSGateway: window.ENV?.IPFS_GATEWAY || process.env.REACT_APP_IPFS_GATEWAY,
   scanUrl: window.ENV?.SCAN_ACCOUNT_URL || process.env.REACT_APP_SCAN_ACCOUNT_URL,
+  quartzRestApiUrl:
+    window.ENV?.NET_QUARTZ_REST_API_URL || process.env.REACT_APP_NET_QUARTZ_REST_API_URL,
   hasuraApiUrl: window.ENV?.HASURA_API_URL || process.env.REACT_APP_HASURA_API_URL,
 };
 
