0c492a67262570764253bcc7c02966ad8115dda3
pasynkov <n@pasynkov.com>
2022-07-06 15:14:29 +0300

    mini fix

--- src/config.ts
+++ src/config.ts
@@ -24,7 +24,7 @@ declare global {
 const chains = getChainList(window.ENV || process.env);
 
 export const config: Config = {
-  IPFSGateway: window.ENV?.IPFS_GATEWAY || process.env.REACT_APP_IPFS_GATEWAY_URL,
+  IPFSGateway: window.ENV?.IPFS_GATEWAY_URL || process.env.REACT_APP_IPFS_GATEWAY_URL,
   scanUrl: window.ENV?.SCAN_ACCOUNT_URL || process.env.REACT_APP_SCAN_ACCOUNT_URL,
   chains,
   defaultChain: chains[getDefaultChain(window.ENV || process.env)],
