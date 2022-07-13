cc20c6af09fc211bbac1f0cd34194d9faa7b1096
Pavel Kalachev <pkalachev@usetech.com>
2022-07-13 14:33:14 +0300

    fix: ENVs

--- src/config.ts
+++ src/config.ts
@@ -5,16 +5,14 @@ import { getChainList, getDefaultChain } from './utils/configParser';
 declare type Env = {
   NET_DEFAULT: string | undefined;
   IPFS_GATEWAY: string | undefined;
-  SCAN_ACCOUNT_URL: string | undefined;
   SUBSCAN_URL: string | undefined;
   UNIQUESCAN_URL: string | undefined;
-  NET_QUARTS_SUBSCAN_ACCOUNT_URL: string | undefined;
+  NET_QUARTZ_SUBSCAN_ACCOUNT_URL: string | undefined;
   NET_UNIQUE_SUBSCAN_ACCOUNT_URL: string | undefined;
 } & Record<string, string | undefined>;
 
 declare type Config = {
   IPFSGateway: string | undefined;
-  scanUrl: string | undefined;
   chains: Record<string, Chain>;
   defaultChain: Chain;
   quartzSubscanUrl: string | undefined;
@@ -32,12 +30,11 @@ const chains = getChainList(window.ENV || process.env);
 
 export const config: Config = {
   IPFSGateway: window.ENV?.IPFS_GATEWAY_URL || process.env.REACT_APP_IPFS_GATEWAY_URL,
-  scanUrl: window.ENV?.SCAN_ACCOUNT_URL || process.env.REACT_APP_SCAN_ACCOUNT_URL,
   chains,
   defaultChain: chains[getDefaultChain(window.ENV || process.env)],
   quartzSubscanUrl:
-    window.ENV?.NET_QUARTS_SUBSCAN_ACCOUNT_URL ||
-    process.env.REACT_APP_NET_QUARTS_SUBSCAN_ACCOUNT_URL,
+    window.ENV?.NET_QUARTZ_SUBSCAN_ACCOUNT_URL ||
+    process.env.REACT_APP_NET_QUARTZ_SUBSCAN_ACCOUNT_URL,
   uniqueSubscanUrl:
     window.ENV?.NET_UNIQUE_SUBSCAN_ACCOUNT_URL ||
     process.env.REACT_APP_NET_UNIQUE_SUBSCAN_ACCOUNT_URL,
