7f1e92a95cdcb93ff59f0af1d48a68c765750e9a
h3lpkey <h3lpkey@gmail.com>
2022-07-18 15:32:59 +0400

    remove switch

--- src/utils/scansUrls.ts
+++ src/utils/scansUrls.ts
@@ -1,26 +1,8 @@
 export const getScanAddresses = (
   network: string,
 ): { urlSubscan: string; urlUniquescan: string } => {
-  switch (network) {
-    case 'UNIQUE':
-      return {
-        urlSubscan: process.env.REACT_APP_NET_UNIQUE_SUBSCAN_ADDRESS || '',
-        urlUniquescan: process.env.REACT_APP_NET_UNIQUE_UNIQUESCAN_ADDRESS || '',
-      };
-    case 'QUARTZ':
-      return {
-        urlSubscan: process.env.REACT_APP_NET_QUARTZ_SUBSCAN_ADDRESS || '',
-        urlUniquescan: process.env.REACT_APP_NET_QUARTZ_UNIQUESCAN_ADDRESS || '',
-      };
-    case 'OPAL':
-      return {
-        urlSubscan: '',
-        urlUniquescan: process.env.REACT_APP_NET_OPAL_UNIQUESCAN_ADDRESS || '',
-      };
-    default:
-      return {
-        urlSubscan: '',
-        urlUniquescan: '',
-      };
-  }
+  return {
+    urlSubscan: process.env[`REACT_APP_NET_${network}_SUBSCAN_ADDRESS`] || '',
+    urlUniquescan: process.env[`REACT_APP_NET_${network}_UNIQUESCAN_ADDRESS`] || '',
+  };
 };
