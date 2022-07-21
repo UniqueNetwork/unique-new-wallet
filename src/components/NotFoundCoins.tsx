836b254c71e78acf4ca1d6fe306d4432c429dcd4
h3lpkey <h3lpkey@gmail.com>
2022-07-21 09:19:42 +0400

    style fixes for mycollection page

--- src/components/NotFoundCoins.tsx
+++ src/components/NotFoundCoins.tsx
@@ -1,7 +1,7 @@
-import React, { VFC } from 'react';
-import classNames from 'classnames';
-import styled from 'styled-components';
 import { Icon } from '@unique-nft/ui-kit';
+import classNames from 'classnames';
+import React, { VFC } from 'react';
+import styled from 'styled-components';
 
 interface NotFoundCoinsProps {
   className?: string;
@@ -21,7 +21,12 @@ export const NotFoundCoins = styled(NotFoundCoinsComponent)`
     align-items: center;
     justify-content: center;
     color: var(--color-blue-grey-500);
-    min-height: 600px;
     grid-row-gap: var(--prop-gap);
+    font-size: 16px;
+    font-weight: 400;
+    min-height: 400px;
+    @media screen and (max-width: 1024px) {
+      min-height: 200px;
+    }
   }
 `;
