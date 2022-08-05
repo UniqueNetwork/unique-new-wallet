e6bba581cc502e9bab507d535eca89a7ab770bcd
h3lpkey <h3lpkey@gmail.com>
2022-08-05 10:41:23 +0400

    refactor coins config

--- src/pages/MyTokens/Coins/Coins.tsx
+++ src/pages/MyTokens/Coins/Coins.tsx
@@ -1,14 +1,14 @@
+import { Heading } from '@unique-nft/ui-kit';
 import React, { useCallback, useEffect, useState, VFC } from 'react';
 import styled from 'styled-components';
-import { Heading } from '@unique-nft/ui-kit';
 
-import { RampModal } from '@app/pages';
-import { Chain, NetworkType } from '@app/types';
-import { useAccounts } from '@app/hooks';
-import { SendFunds } from '@app/pages/SendFunds';
+import { ApiWrapper } from '@app/api';
 import { useAccountBalancesService } from '@app/api/restApi/balance/hooks/useAccountBalancesService';
 import { config } from '@app/config';
-import { ApiWrapper } from '@app/api';
+import { useAccounts } from '@app/hooks';
+import { RampModal } from '@app/pages';
+import { SendFunds } from '@app/pages/SendFunds';
+import { Chain, NetworkType } from '@app/types';
 import { logUserEvent, UserEvents } from '@app/utils/logUserEvent';
 
 import { CoinsRow } from './components';
@@ -55,31 +55,24 @@ export const CoinsComponent: VFC<CoinsComponentProps> = ({ className }) => {
     [],
   );
 
-  const getCoinRowConfig = (chain: string) => {
-    switch (chain) {
-      case 'OPAL':
-        return {
-          getDisabled: false,
-          onGet: () => {
-            window.open(config.telegramBot, '_blank', 'noopener');
-          },
-        };
-      case 'KUSAMA':
-        return {
-          getDisabled: false,
-          onGet: getCoinsHandler,
-        };
-      case 'QUARTZ':
-        return {
-          getDisabled: true,
-          onGet: undefined,
-        };
-      default:
-        return {
-          getDisabled: true,
-          onGet: undefined,
-        };
-    }
+  interface NetworkInfo {
+    getDisabled: boolean;
+    onGet?: () => void;
+  }
+
+  type NetworkName = 'OPAL' | 'KUSAMA' | 'QUARTZ' | 'UNIQUE' | 'POLKADOT' | string;
+
+  const coinConfig: Record<NetworkName, NetworkInfo> = {
+    OPAL: {
+      getDisabled: false,
+      onGet: () => {
+        window.open(config.telegramBot, '_blank', 'noopener');
+      },
+    },
+    KUSAMA: { getDisabled: false, onGet: getCoinsHandler },
+    QUARTZ: { getDisabled: true },
+    UNIQUE: { getDisabled: true },
+    POLKADOT: { getDisabled: true },
   };
 
   return (
@@ -100,27 +93,31 @@ export const CoinsComponent: VFC<CoinsComponentProps> = ({ className }) => {
         <Heading size="4">Network</Heading>
         {Object.values(config.chains).map((chain, idx) => {
           const isQuartzChain = chain.network.toLowerCase() === 'quartz';
-          const { getDisabled, onGet } = getCoinRowConfig(chain.network);
-          return (
-            <CoinsRow
-              getDisabled={getDisabled}
-              key={chain.network}
-              loading={chainsBalanceLoading}
-              sendDisabled={
-                isQuartzChain || !Number(chainsBalance?.[idx].availableBalance.amount)
-              }
-              address={selectedAccount?.address}
-              balanceFull={chainsBalance?.[idx].freeBalance.amount}
-              balanceLocked={chainsBalance?.[idx].lockedBalance.amount}
-              balanceTransferable={chainsBalance?.[idx].availableBalance.amount}
-              iconName={`chain-${chain.network.toLowerCase()}`}
-              name={chain.name}
-              symbol={chainsBalance?.[idx].availableBalance.unit}
-              chain={chain}
-              onSend={sendFundsHandler}
-              onGet={onGet}
-            />
-          );
+          if (coinConfig[chain.network]) {
+            const { getDisabled, onGet } = coinConfig[chain.network];
+            return (
+              <CoinsRow
+                getDisabled={getDisabled}
+                key={chain.network}
+                loading={chainsBalanceLoading}
+                sendDisabled={
+                  isQuartzChain || !Number(chainsBalance?.[idx].availableBalance.amount)
+                }
+                address={selectedAccount?.address}
+                balanceFull={chainsBalance?.[idx].freeBalance.amount}
+                balanceLocked={chainsBalance?.[idx].lockedBalance.amount}
+                balanceTransferable={chainsBalance?.[idx].availableBalance.amount}
+                iconName={`chain-${chain.network.toLowerCase()}`}
+                name={chain.name}
+                symbol={chainsBalance?.[idx].availableBalance.unit}
+                chain={chain}
+                onSend={sendFundsHandler}
+                onGet={onGet}
+              />
+            );
+          } else {
+            return null;
+          }
         })}
       </CoinsContainer>
     </>
