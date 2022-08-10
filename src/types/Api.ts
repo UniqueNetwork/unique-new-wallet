20b5f0300ede323245a420284d0557e4764ae551
Kozlov Sergey <skozlov@usetech.ru>
2022-08-10 18:01:40 +0300

    fix: [WAL-489] - error occurs while transferring NFT

--- src/types/Api.ts
+++ src/types/Api.ts
@@ -139,6 +139,8 @@ export interface TransferTokenBody {
    * @example yGCyN3eydMkze4EPtz59Tn7obwbUbYNZCz48dp8FRdemTaLwm
    */
   to: string;
+
+  address: string;
 }
 
 export interface CollectionSponsorship {
