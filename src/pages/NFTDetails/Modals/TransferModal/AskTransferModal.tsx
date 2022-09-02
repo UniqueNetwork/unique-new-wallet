e97f59241a3a24259457969db52b2c7bfcfefbde
Andrew Ov <anr.andrew@gmail.com>
2022-09-02 17:32:18 +0400

    [WAL-503] Transfer modal (#220)
    Co-authored-by: Andrey O <aovsyannikov@usetech.com>

--- src/pages/NFTDetails/Modals/TransferModal/AskTransferModal.tsx
+++ src/pages/NFTDetails/Modals/TransferModal/AskTransferModal.tsx
@@ -1,9 +1,19 @@
-import { VFC } from 'react';
-import { Button, Heading, InputText, Modal } from '@unique-nft/ui-kit';
+import { createRef, VFC } from 'react';
 import styled from 'styled-components';
+import {
+  Alert,
+  Button,
+  Heading,
+  Icon,
+  InputText,
+  Modal,
+  Text,
+  Tooltip,
+} from '@unique-nft/ui-kit';
 
 import { FeeInformationTransaction } from '@app/components/FeeInformationTransaction';
-import { Alert } from '@app/components/Alert';
+import { ModalHeader } from '@app/pages/Accounts/Modals/commonComponents';
+import { ModalContent, ModalFooter } from '@app/pages/components/ModalComponents';
 
 interface AskTransferModalProps {
   fee?: string;
@@ -14,6 +24,21 @@ interface AskTransferModalProps {
   onRecipientChange: (recipient: string) => void;
 }
 
+const TransferRow = styled.div`
+  margin-bottom: calc(var(--prop-gap) * 1.5);
+
+  .unique-input-text {
+    width: 100%;
+
+    label {
+      display: flex;
+      align-items: center;
+      margin-bottom: var(--prop-gap);
+      gap: calc(var(--prop-gap) / 2.5);
+    }
+  }
+`;
+
 export const AskTransferModal: VFC<AskTransferModalProps> = ({
   fee,
   isVisible,
@@ -22,66 +47,70 @@ export const AskTransferModal: VFC<AskTransferModalProps> = ({
   onConfirm,
   onRecipientChange,
 }) => {
+  const tooltipRef = createRef<HTMLDivElement>();
+
   return (
     <Modal isClosable isVisible={isVisible} onClose={onClose}>
-      <HeadingWrapper>
+      <ModalHeader>
         <Heading size="2">Transfer NFT</Heading>
-      </HeadingWrapper>
-      <InputWrapper
-        label="Please enter the address you wish to send the NFT to"
-        maxLength={49}
-        value={recipient}
-        onChange={onRecipientChange}
-      />
-
-      <FeeInformationTransaction fee={fee} />
-      {!fee && (
-        <AlertStyle type="warning">
-          A fee will be calculated after entering the address
-        </AlertStyle>
-      )}
-      <AlertStyle type="warning">
-        Proceed with caution, once confirmed the transaction cannot be reverted.
-      </AlertStyle>
-      <AlertStyle type="warning">
-        Make sure to use a Substrate address created with a Polkadot.&#123;js&#125;
-        wallet. There is no guarantee that third-party wallets, exchanges or hardware
-        wallets can successfully sign and process your transfer which will result in a
-        possible loss of the NFT.
-      </AlertStyle>
-      <ButtonWrapper>
+      </ModalHeader>
+      <ModalContent>
+        <TransferRow>
+          <InputText
+            label={
+              <>
+                <Tooltip
+                  align={{
+                    appearance: 'horizontal',
+                    horizontal: 'right',
+                    vertical: 'top',
+                  }}
+                  targetRef={tooltipRef}
+                >
+                  Make sure to&nbsp;use a&nbsp;Substrate address created with
+                  a&nbsp;Polkadot.&#123;js&#125; wallet. There is&nbsp;no&nbsp;guarantee
+                  that third-party wallets, exchanges or&nbsp;hardware wallets can
+                  successfully sign and process your transfer which will result
+                  in&nbsp;a&nbsp;possible loss of&nbsp;the NFT.
+                </Tooltip>
+                Recipient address
+                <Icon
+                  color="var(--color-primary-500)"
+                  name="question"
+                  ref={tooltipRef}
+                  size={24}
+                />
+              </>
+            }
+            maxLength={49}
+            value={recipient}
+            onChange={onRecipientChange}
+          />
+        </TransferRow>
+        <TransferRow>
+          <Text>
+            Proceed with caution, once confirmed the transaction cannot be reverted.
+          </Text>
+        </TransferRow>
+        <TransferRow>
+          <FeeInformationTransaction fee={fee} />
+        </TransferRow>
+        <TransferRow>
+          {!fee && (
+            <Alert type="warning">
+              A fee will be calculated after entering the address
+            </Alert>
+          )}
+        </TransferRow>
+      </ModalContent>
+      <ModalFooter>
         <Button
           role="primary"
           title="Confirm"
           disabled={!recipient}
           onClick={onConfirm}
         />
-      </ButtonWrapper>
+      </ModalFooter>
     </Modal>
   );
 };
-
-const AlertStyle = styled(Alert)`
-  margin-top: var(--prop-gap);
-`;
-
-const InputWrapper = styled(InputText)`
-  margin-bottom: calc(var(--prop-gap) * 2);
-  width: 100%;
-
-  label {
-    margin-bottom: var(--prop-gap);
-  }
-`;
-
-const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  margin-top: calc(var(--prop-gap) * 1.5);
-`;
-
-const HeadingWrapper = styled.div`
-  && h2 {
-    margin-bottom: calc(var(--prop-gap) * 2);
-  }
-`;
