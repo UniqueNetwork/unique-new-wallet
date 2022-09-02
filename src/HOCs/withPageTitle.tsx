844235a371df0db2902f87ac1905c1d7a49b4a3d
Andrew Ov <anr.andrew@gmail.com>
2022-09-02 17:39:20 +0400

    [WAL-497] Back link icon as Icon component (#210)
    Co-authored-by: Andrey O <aovsyannikov@usetech.com>

--- src/HOCs/withPageTitle.tsx
+++ src/HOCs/withPageTitle.tsx
@@ -1,6 +1,9 @@
-/* eslint-disable react/display-name */
-import React, { ComponentType, useEffect } from 'react';
+import { ComponentType, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Icon, Text } from '@unique-nft/ui-kit';
 
+import { useApi } from '@app/hooks';
 import { usePageSettingContext } from '@app/context';
 
 type TitleConfig = {
@@ -8,21 +11,29 @@ type TitleConfig = {
   backLink?: string;
 };
 
+const BackLink = styled(Link)`
+  display: inline-flex;
+  align-items: center;
+  gap: calc(var(--prop-gap) / 4);
+`;
+
 export const withPageTitle =
   ({ header, backLink }: TitleConfig) =>
   <P,>(Page: ComponentType<P>) =>
   (props: P) => {
+    const { currentChain } = useApi();
     const { setPageBreadcrumbs, setPageHeading } = usePageSettingContext();
 
     useEffect(() => {
-      const options = backLink
-        ? [
-            {
-              title: '🡠 back',
-              link: backLink,
-            },
-          ]
-        : [];
+      const link = (
+        <BackLink to={`/${currentChain?.network}/${backLink}`}>
+          <Icon color="var(--color-blue-grey-500)" name="arrow-left" size={16} />
+          <Text color="blue-grey-500" weight="light">
+            back
+          </Text>
+        </BackLink>
+      );
+      const options = backLink ? [link] : [];
 
       setPageBreadcrumbs({
         options,
