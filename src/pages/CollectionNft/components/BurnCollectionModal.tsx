d6451685723cdf90303b658ae74583b6a5a22374
Sergey Kozlov <sergeykozlov.2011@yandex.ru>
2022-10-04 18:16:51 +0300

    feat: [WAL-543, WAL-545, WAL-548] - adaptive modal (#259)
    Co-authored-by: Kozlov Sergey <skozlov@usetech.ru>

--- src/pages/CollectionNft/components/BurnCollectionModal.tsx
+++ src/pages/CollectionNft/components/BurnCollectionModal.tsx
@@ -1,10 +1,13 @@
 import { Button, Text } from '@unique-nft/ui-kit';
 
 import { Modal, ModalProps } from '@app/components/Modal';
-import { ContentRow, ModalFooter } from '@app/pages/components/ModalComponents';
+import { ContentRow } from '@app/pages/components/ModalComponents';
 import { FeeInformationTransaction } from '@app/components/FeeInformationTransaction';
 
-type BurnCollectionModalProps = Omit<ModalProps, 'children'> & {
+type BurnCollectionModalProps = Omit<
+  ModalProps,
+  'children' | 'footerButtons' | 'title'
+> & {
   onConfirm(): void;
 };
 
@@ -12,15 +15,16 @@ export const BurnCollectionModal = ({
   onConfirm,
   ...modalProps
 }: BurnCollectionModalProps) => (
-  <Modal title="Burn collection" {...modalProps}>
+  <Modal
+    title="Burn collection"
+    footerButtons={<Button role="primary" title="Confirm" onClick={onConfirm} />}
+    {...modalProps}
+  >
     <ContentRow>
       <Text>You will not be able to undo this action.</Text>
     </ContentRow>
     <ContentRow>
       <FeeInformationTransaction fee="2.073447 QTZ" />
     </ContentRow>
-    <ModalFooter>
-      <Button role="primary" title="Confirm" onClick={onConfirm} />
-    </ModalFooter>
   </Modal>
 );
