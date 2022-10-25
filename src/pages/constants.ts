1c7050a3ea4fcc846d42da416521ce22a43929b1
pavlyxar <pavlyxar567@inbox.ru>
2022-10-25 18:01:10 +0300

    feat: add max byte size validation of symbol field and dynamic changing of available fields maxlength

--- src/pages/constants.ts
+++ src/pages/constants.ts
@@ -1,3 +1,7 @@
 export const _10MB = 10000000;
+export const MAX_NAME_SIZE = 64;
+export const MAX_DESCRIPTION_SIZE = 256;
+export const MAX_SYMBOL_SIZE = 4;
+export const MAX_SYMBOL_BYTES_SIZE = 16;
 export const FILE_SIZE_LIMIT_ERROR = 'File size more than 10MB';
 export const NO_BALANCE_MESSAGE = 'Insufficient funds. Please top up\u00a0your balance';
