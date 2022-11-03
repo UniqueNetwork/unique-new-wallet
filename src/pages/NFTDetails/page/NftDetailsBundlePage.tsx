97e190654c2c2d29e90a5febc1c388f9c661c33a
Andrew Ov <anr.andrew@gmail.com>
2022-11-03 18:10:14 +0600

    fix: [WAL-563] - My tokens, Token Page adaptive. (#299)
    Co-authored-by: Andrey O <aovsyannikov@usetech.com>

--- src/pages/NFTDetails/page/NftDetailsBundlePage.tsx
+++ src/pages/NFTDetails/page/NftDetailsBundlePage.tsx
@@ -17,7 +17,6 @@ import { useIsOwner } from '@app/pages/NFTDetails/hooks/useIsOwner';
 import { countNestedChildren } from '@app/components/BundleTree/helpers-bundle';
 import { TransferBtn } from '@app/components';
 import { logUserEvent, UserEvents } from '@app/utils/logUserEvent';
-import { NftDetailsWrapperButtons } from '@app/pages/NFTDetails/components';
 import { BottomBar, BottomBarHeader } from '@app/pages/components/BottomBar';
 
 const areNodesEqual = (a: INestingToken, b: INestingToken) =>
@@ -220,7 +219,6 @@ export const NftDetailsBundlePage = () => {
     />
   );
 
-  // className={classNames({hidden: isShowBundleTreeMobile && deviceSize === DeviceSize.xs})}
   return (
     <NftDetailsWrapper>
       <NftDetailsLayout
@@ -234,9 +232,8 @@ export const NftDetailsBundlePage = () => {
           achievement={isBundleToken() ? 'Bundle' : 'Nested'}
           buttons={
             isOwner && (
-              <NftDetailsWrapperButtons>
+              <>
                 <TransferBtn
-                  className="transfer-btn"
                   wide={deviceSize === DeviceSize.xs}
                   title="Transfer"
                   role="primary"
@@ -250,7 +247,7 @@ export const NftDetailsBundlePage = () => {
                     onClick={unnestTokenAction}
                   />
                 )}
-              </NftDetailsWrapperButtons>
+              </>
             )
           }
           onCurrentModal={setCurrentModal}
