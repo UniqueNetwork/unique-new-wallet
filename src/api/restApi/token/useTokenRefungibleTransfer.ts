995f8111710eae63c640524202539dece562b6a6
drgosh <drgosh.m@gmail.com>
2023-03-15 14:59:57 +0700

    Fixed transfer and burning RFT from metamask account

--- src/api/restApi/token/useTokenRefungibleTransfer.ts
+++ src/api/restApi/token/useTokenRefungibleTransfer.ts
@@ -1,8 +1,18 @@
+import { MetamaskAccountName } from '@app/account/MetamaskWallet';
 import { useExtrinsicMutation } from '@app/api';
-import { useApi } from '@app/hooks';
+import { useAccounts, useApi } from '@app/hooks';
+import { useMetamaskTransferRefungibleToken } from '@app/hooks/useMetamask/useMetamaskTransferRefungibleToken';
 
 export const useTokenRefungibleTransfer = () => {
   const { api } = useApi();
+  const { selectedAccount } = useAccounts();
 
-  return useExtrinsicMutation(api.refungible.transferToken);
+  const defaultTokensTransfer = useExtrinsicMutation(api.refungible.transferToken);
+  const metamaskTokensTransfer = useMetamaskTransferRefungibleToken();
+
+  if (selectedAccount?.name === MetamaskAccountName) {
+    return metamaskTokensTransfer;
+  }
+
+  return defaultTokensTransfer;
 };
