ccc3c0b3ab9fbe4d3d682f9ee793638d0854c114
drgosh <drgosh.m@gmail.com>
2023-03-15 11:46:55 +0700

    Implemented withdrawing balance from the ethereum mirror

--- src/api/restApi/balance/index.ts
+++ src/api/restApi/balance/index.ts
@@ -2,3 +2,5 @@ export * from './useAccountBalanceService';
 export * from './useAccountBalancesService';
 export * from './useAccountBalanceTransfer';
 export * from './useAccountsBalanceService';
+export * from './useAccountWithdrawableBalanceService';
+export * from './useAccountBalanceWithdraw';
