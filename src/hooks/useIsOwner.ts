0a4db4c3c7be6656f6b7be26d37eced99bf75a18
drgosh <drgosh.m@gmail.com>
2023-04-03 21:31:09 +0700

    Fixed nested RFT on the collection detail page

---
+++ src/hooks/useIsOwner.ts
@@ -0,0 +1,67 @@
+import { useMemo } from 'react';
+import { Address } from '@unique-nft/utils/address';
+import { CollectionInfoWithSchemaResponse, TokenByIdResponse } from '@unique-nft/sdk';
+
+import { useAccounts } from '@app/hooks';
+import { useTokenGetBalance } from '@app/api/restApi/token/useTokenGetBalance';
+import { useTokenTopmostOwner } from '@app/api/restApi/token/useTokenTopmostOwner';
+
+export const useIsOwner = (
+  token:
+    | (Pick<TokenByIdResponse, 'collectionId' | 'tokenId' | 'owner'> & {
+        collection: Pick<CollectionInfoWithSchemaResponse, 'mode'>;
+        amount?: string;
+      })
+    | undefined,
+  parentAddress?: string,
+  options?: { checkNesting?: boolean },
+) => {
+  const { checkNesting } = options || { checkNesting: true };
+  const { selectedAccount } = useAccounts();
+
+  const parent =
+    checkNesting && Address.is.nestingAddress(parentAddress || '')
+      ? Address.nesting.addressToIds(parentAddress || '')
+      : undefined;
+
+  const { data: topmostAccount } = useTokenTopmostOwner({
+    collectionId: parent?.collectionId,
+    tokenId: parent?.tokenId,
+  });
+
+  const { data: fractionsBalance } = useTokenGetBalance({
+    tokenId: token?.tokenId,
+    collectionId: token?.collectionId,
+    address: selectedAccount?.address,
+    isFractional: !token?.amount && token?.collection.mode === 'ReFungible',
+  });
+
+  return useMemo(() => {
+    if (topmostAccount?.topmostOwner) {
+      return topmostAccount.topmostOwner === selectedAccount?.address;
+    }
+
+    if (!token?.amount && fractionsBalance?.amount) {
+      return true;
+    }
+
+    if (!token?.owner || !selectedAccount?.address) {
+      return false;
+    }
+    const address = token.owner;
+
+    if (
+      Address.is.ethereumAddress(token.owner) &&
+      !Address.is.ethereumAddress(selectedAccount.address)
+    ) {
+      return (
+        Address.mirror.substrateToEthereum(selectedAccount.address).toLowerCase() ===
+        token.owner
+      );
+    }
+    return (
+      Address.extract.addressNormalized(address) ===
+      Address.extract.addressNormalized(selectedAccount.address)
+    );
+  }, [selectedAccount?.address, token?.owner, fractionsBalance, topmostAccount]);
+};
