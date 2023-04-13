135bad69e64893c5281793f68f10dd3697a78fd9
drgosh <drgosh.m@gmail.com>
2023-04-13 20:00:12 +0700

    Fixed withdraw amount format

--- src/pages/MyTokens/Coins/components/CoinsRow.tsx
+++ src/pages/MyTokens/Coins/components/CoinsRow.tsx
@@ -1,16 +1,15 @@
 import React, { memo, VFC } from 'react';
 import classNames from 'classnames';
 import styled from 'styled-components';
-import { Loader } from '@unique-nft/ui-kit';
 import { BalanceResponse } from '@unique-nft/sdk';
 
 import { Chain } from '@app/types';
 import { logUserEvent, UserEvents } from '@app/utils/logUserEvent';
-import { Button, TransferBtn } from '@app/components';
+import { Button, TransferBtn, Loader } from '@app/components';
 import { NetworkBalances, TNetworkBalances } from '@app/pages/components/NetworkBalances';
 import AccountCard from '@app/pages/Accounts/components/AccountCard';
 import { DeviceSize, useDeviceSize } from '@app/hooks';
-import { formatAmount, formatKusamaBalance, shortAddress } from '@app/utils';
+import { shortAddress } from '@app/utils';
 
 type CoinsRowComponentProps = TNetworkBalances & {
   balanceToWithdraw?: BalanceResponse;
@@ -146,9 +145,10 @@ export const CoinsRowComponent: VFC<CoinsRowComponentProps> = (props) => {
               }}
             />
           </SendGetWrapper>
+
           {balanceToWithdraw?.raw && balanceToWithdraw?.raw !== '0' && (
             <TransferBtn
-              title={`Withdraw ${balanceToWithdraw?.formatted || ''} ${symbol}`}
+              title={`Withdraw ${balanceToWithdraw?.amount || ''} ${symbol}`}
               onClick={() => {
                 logUserEvent(`${UserEvents.WITHDRAW_COINS}_${symbol}`);
                 onWithdraw(symbol, chain, balanceToWithdraw?.raw || '0');
